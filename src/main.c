333d6b0172770d208b49257f6a152369a878a1d3
Gerd Kohlberger <gerdk@src.gnome.org>
2013-10-28 09:33:00 +0100

    Change config.h include

--- src/main.c
+++ src/main.c
@@ -17,9 +17,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <config.h>
 #include <glib/gi18n.h>
 
+#include "config.h"
 #include "mt_application.h"
 
 int
